d5ba2cd13ad4b42a61f206c8ab7ccca79f696b50
Hameed M <s201424820@gmail.com>
2024-06-13 02:08:08 +0300

    test input type

--- src/app/tds/tds.component.ts
+++ src/app/tds/tds.component.ts
@@ -5,10 +5,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 @Component({
   selector: 'app-tds',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, CommonModule, FormsModule, MatCardModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatButton,
+  ],
   templateUrl: './tds.component.html',
   styleUrl: './tds.component.css',
 })
@@ -16,6 +25,7 @@ export class TdsComponent {
   w1: any = null;
   w2: any = null;
   vol: any = 0;
+  VirtualKeyboardPolicy: any;
 
   get diff() {
     return Math.abs(this.w1 - this.w2);
@@ -26,7 +36,6 @@ export class TdsComponent {
   }
 
   get tds_percent() {
-    return (this.tds_ppm) / 1000;
+    return this.tds_ppm / 1000;
   }
-
 }
